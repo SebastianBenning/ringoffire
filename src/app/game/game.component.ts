5fbc7956063b8ba8230a5a6cd4dd37c5f8bdffb7
SebastianBenning <benning655@googlemail.com>
2023-02-03 12:28:38 +0100

    Cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
-  pickCardAnimation= false;
-  takeCard(){
-    this.pickCardAnimation = true;
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+  }
+
+  ngOnInit(): void {
+    this.newGame()
+  }
+
+  newGame() {
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+      
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 
 }
